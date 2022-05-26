205dfc5585d4681cf3ade0cddc83d9774373f222
space cadet grant <53055825+mcnugboy@users.noreply.github.com>
2022-05-26 09:08:01 -0500

    saved.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() { 
   var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
+  listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task."; 
   tasksToDoEl.appendChild(listItemEl); 
   }; 
